5efc5d1313262241ee2eac7615eac62f10188194
Yashvendra Deep Dwivedi <yashvendradeep@gmail.com>
2021-03-03 15:45:00 +0530

    under construction

---
+++ main.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    var tens = 00;
+    var sec = 00;
+    var Tens = document.getElementById("tens")
+    var Sec = document.getElementById("seconds")
+    var start = document.getElementById("button-start")
+    var stop = document.getElementsByTagName("button-stop")
+    var reset = document.getElementById("button-reset");
+    var interval;
+}
